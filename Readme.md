32d7a0df4aba1e32254f1aed8d23e1850dc9c008
Moe Hodroj <54459766+mrtechnology88@users.noreply.github.com>
2020-12-23 23:30:50 -0500

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,1 @@
+To create this app (Quiz Application with Timer). First, i created four Files one HTML File, CSS File and the other two are JavaScript files. I wanted to add the score table, but unfortunately i ran out of time. So am planning to add this later. I commented inside the files for better understanding of the code. 
---
+++ js/questions.js
@@ -0,0 +1,119 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
+    answer: "The User's machine running a Web browser",
+    options: [
+      "The User's machine running a Web browser",
+      "The Web server",
+      "A central machine deep within Netscape's corporate offices",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which is not a new global attribute in HTML5?",
+    answer: "Title",
+    options: [
+      "Contenteditable",
+      "Contextmenu",
+      "Title",
+      "Draggable"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does main include?",
+    answer: "Article",
+    options: [
+      "Header",
+      "Sidebar",
+      "Article",
+      "Footer"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What are variables used for in JavaScript Programs?",
+    answer: "Storing numbers, dates, or other values",
+    options: [
+      "Storing numbers, dates, or other values",
+      "Varying randomly",
+      "Causing high-school algebra flashbacks",
+      "None of the above"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Which of the following is not a valid JavaScript variable name?",
+    answer: "2names",
+    options: [
+      "_first_and_last_names",
+      "2names",
+      "FirstAndLast",
+      "None of the above"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Which tag is used to encapsulate navigation and then style the elements appropriately as menu items?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "ul",
+      "li",
+      "nav",
+      "both a and b"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "If we want define style for an unique element, then which css selector will we use ?",
+    answer: "Id",
+    options: [
+      "Id",
+      "class",
+      "text",
+      "name"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "If we want to wrap a block of text around an image, which css property will we use ?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "wrap",
+      "float",
+      "align",
+      "push"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "If we want to use a nice looking green dotted border around an image, which css property will we use?",
+    answer: "border-style",
+    options: [
+      "border-color",
+      "border-style",
+      "border-line",
+      "border-decoration"
+    ]
+  },
+
+]
